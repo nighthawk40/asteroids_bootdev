18bce259572c55fda2d018452a84685119d5660a
nighthawk40 <jacobgogel@att.net>
2025-08-21 00:27:51 -0400

    Implement shooting mechanics and collision detection for player and asteroids

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import pygame 
+import pygame
+import sys  
 from constants import * 
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     # Intialize Pygame
@@ -18,13 +20,14 @@ def main():
     # updatable group will contain objects that need to be updated each frame
     # drawable group will contain objects that need to be drawn each frame
     # We will use these groups to manage the player and other game objects
-    # We will also use these groups to manage the player and other game objects
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
 
     # Create player instance
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -41,6 +44,12 @@ def main():
         # Update game state
         updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+                
+            
         # fill screen with black
         screen.fill((0, 0, 0))
 
